78ddd604e6d18f04d9c1a6acb1d485a8d715941e
josedallatorre <jose.dallatorre@hotmail.com>
2025-08-19 16:01:35 +0200

    fix: ignoring build folder

--- build/pitchside_tracker/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/jota/Desktop/uni/magistrale/droni/pitchside_tracker/install/pitchside_tracker'
