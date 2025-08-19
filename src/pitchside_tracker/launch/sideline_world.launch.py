69e111d067102e3f4b70c6d2d68d8b73c21bacd9
josedallatorre <jose.dallatorre@hotmail.com>
2025-08-19 16:03:36 +0200

    feat: launch file spawn gazebo correctly

--- src/pitchside_tracker/launch/sideline_world.launch.py
+++ src/pitchside_tracker/launch/sideline_world.launch.py
@@ -1,24 +1,38 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, SetEnvironmentVariable
+from launch_ros.actions import Node
+from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
 
 def generate_launch_description():
-    pkg_share = get_package_share_directory('pitchside_tracker')
-    world_path = os.path.join(pkg_share, 'worlds', 'football_pitch.world')
-    models_path = os.path.join(pkg_share, 'models')
-
-    set_model_path = SetEnvironmentVariable(name='GZ_MODEL_PATH', value=models_path)
-
-    ros_gz_launch = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('ros_gz'), 'launch', 'gz.launch.py')
-        ),
-        launch_arguments={'gz_args': world_path}.items()
-    )
+    ros_gz_sim_pkg_path = FindPackageShare('ros_gz_sim')
+    example_pkg_path = FindPackageShare('pitchside_tracker')  # Replace with your own package name
+    gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
 
     return LaunchDescription([
-        set_model_path,
-        ros_gz_launch,
+        SetEnvironmentVariable(
+            'GZ_SIM_RESOURCE_PATH',
+            PathJoinSubstitution([example_pkg_path, 'models'])
+        ),
+        SetEnvironmentVariable(
+            'GZ_SIM_PLUGIN_PATH',
+            PathJoinSubstitution([example_pkg_path, 'plugins'])
+        ),
+        Node(
+            package='pitchside_tracker',
+            namespace='pitchside_tracker',
+            executable='tracking_node',
+            name='sim_tracking_node',
+            output='screen',
+        ),
+        IncludeLaunchDescription(
+            PythonLaunchDescriptionSource(gz_launch_path),
+            launch_arguments={
+                'gz_args': [PathJoinSubstitution([example_pkg_path, 'worlds/football_pitch.world'])],  # Replace with your own world file
+                'on_exit_shutdown': 'True'
+            }.items(),
+        ),
+
     ])
