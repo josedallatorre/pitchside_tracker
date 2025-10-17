61921ad00bba65a6189e251462ab1dc90f079c83
josedallatorre <jose.dallatorre@hotmail.com>
2025-10-17 18:52:18 +0200

    feat: adding sjtu drone

---
+++ src/sjtu_drone/run_docker.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+usage(){
+    echo "Usage: $0 [-r <humble|iron|rolling>]"
+    exit 1
+}
+
+
+XSOCK=/tmp/.X11-unix
+XAUTH=$HOME/.Xauthority
+
+xhost +local:docker
+docker run \
+    -it --rm \
+    $VOLUMES \
+    -v ${XSOCK}:${XSOCK} \
+    -v ${XAUTH}:${XAUTH} \
+    -e DISPLAY=${DISPLAY} \
+    -e XAUTHORITY=${XAUTH} \
+    --env=QT_X11_NO_MITSHM=1 \
+    --privileged \
+    --net=host \
+    --name="sjtu_drone" \
+    josedallatorre/sjtu_drone
+xhost -local:docker
