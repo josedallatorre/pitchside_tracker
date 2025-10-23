6d4d55a6b8387ba3a2ba19b6c148dc59101e8d34
josedallatorre <jose.dallatorre@hotmail.com>
2025-10-23 22:33:34 +0200

    feat: discovery for the subdir for the model

--- src/sjtu_drone/sjtu_drone_bringup/setup.py
+++ src/sjtu_drone/sjtu_drone_bringup/setup.py
@@ -1,8 +1,19 @@
 from setuptools import setup
-from glob import glob
+import glob
 import os
 
 package_name = 'sjtu_drone_bringup'
+
+
+# Helper to flatten files in a directory
+def get_data_files(src_folder, dst_folder):
+    files = []
+    for path in glob.glob(f'{src_folder}/**', recursive=True):
+        if os.path.isfile(path):
+            rel_path = os.path.relpath(path, src_folder)
+            dest_path = os.path.join(dst_folder, os.path.dirname(rel_path))
+            files.append((dest_path, [path]))
+    return files
 
 setup(
     name=package_name,
@@ -12,10 +23,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, "launch"), glob('launch/*launch.[pxy][yma]*')),
-        (os.path.join('share', package_name, "rviz"), glob('rviz/*.rviz')),
-        (os.path.join('share', package_name, "config"), glob('config/*.yaml')),
-    ],
+        (os.path.join('share', package_name, "launch"), glob.glob('launch/*launch.[pxy][yma]*')),
+        (os.path.join('share', package_name, "rviz"), glob.glob('rviz/*.rviz')),
+        (os.path.join('share', package_name, "config"), glob.glob('config/*.yaml')),
+    ] + get_data_files('models', f'share/{package_name}/models') \
+      + get_data_files('worlds', f'share/{package_name}/worlds'),
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ubuntu',
