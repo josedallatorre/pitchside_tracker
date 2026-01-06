728ef125e2dae453050b126eac7b12c5e52e26bd
josedallatorre <jose.dallatorre@hotmail.com>
2026-01-06 23:45:18 +0100

    fix: renaming

--- src/sjtu_drone/pitchside_tracker/launch/sideline_world.launch.py
+++ src/sjtu_drone/pitchside_tracker/launch/sideline_world.launch.py
@@ -1,38 +1,16 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-from launch.actions import SetEnvironmentVariable, IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import PathJoinSubstitution
-from launch_ros.substitutions import FindPackageShare
 
 
 def generate_launch_description():
-    ros_gz_sim_pkg_path = FindPackageShare('ros_gz_sim')
-    example_pkg_path = FindPackageShare('pitchside_tracker')  # Replace with your own package name
-    gz_launch_path = PathJoinSubstitution([ros_gz_sim_pkg_path, 'launch', 'gz_sim.launch.py'])
-
     return LaunchDescription([
-        SetEnvironmentVariable(
-            'GZ_SIM_RESOURCE_PATH',
-            PathJoinSubstitution([example_pkg_path, 'models'])
-        ),
-        SetEnvironmentVariable(
-            'GZ_SIM_PLUGIN_PATH',
-            PathJoinSubstitution([example_pkg_path, 'plugins'])
-        ),
         Node(
             package='pitchside_tracker',
-            namespace='pitchside_tracker',
-            executable='tracking_node',
-            name='sim_tracking_node',
-            output='screen',
-        ),
-        IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(gz_launch_path),
-            launch_arguments={
-                'gz_args': [PathJoinSubstitution([example_pkg_path, 'worlds/football_pitch.world'])],  # Replace with your own world file
-                'on_exit_shutdown': 'True'
-            }.items(),
-        ),
-
+            executable='image_viewer',
+            name='image_viewer',
+            parameters=[{
+                'image_topic': '/simple_drone/front/image_raw'
+            }],
+            output='screen'
+        )
     ])
