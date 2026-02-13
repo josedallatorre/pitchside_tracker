4a5f2f91e97a958131136894f5c70b26bc6992ec
josedallatorre <jose.dallatorre@hotmail.com>
2026-02-13 11:42:43 +0100

    fix: unused parameters

--- src/sjtu_drone_bringup/launch/sjtu_drone_bringup.launch.py
+++ src/sjtu_drone_bringup/launch/sjtu_drone_bringup.launch.py
@@ -126,11 +126,6 @@ def generate_launch_description():
             package='pitchside_tracker',
             executable='yolo_ball_tracker',
             name='yolo_ball_tracker',
-            parameters=[{
-                'image_topic': '/simple_drone/front/image_raw',
-                'model_path': 'yolov8n.pt',
-                'confidence': 0.4
-            }],
             output='screen'
         ),
         Node(
