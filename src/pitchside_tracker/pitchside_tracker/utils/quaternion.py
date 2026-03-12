ed3b9d77dc2d9d45c29eae20be4b8b5315152673
josedallatorre <jose.dallatorre@hotmail.com>
2026-03-12 17:50:52 +0100

    feat: using quaternion to orient players 2

---
+++ src/pitchside_tracker/pitchside_tracker/utils/quaternion.py
@@ -0,0 +1,18 @@
+import math
+
+def quaternion_from_euler(roll, pitch, yaw):
+    """Convert Euler angles to quaternion (x, y, z, w)."""
+    yaw = math.radians(yaw) # first we convert from degress to radians
+    cy = math.cos(yaw * 0.5)
+    sy = math.sin(yaw * 0.5)
+    cp = math.cos(pitch * 0.5)
+    sp = math.sin(pitch * 0.5)
+    cr = math.cos(roll * 0.5)
+    sr = math.sin(roll * 0.5)
+
+    qw = cr * cp * cy + sr * sp * sy
+    qx = sr * cp * cy - cr * sp * sy
+    qy = cr * sp * cy + sr * cp * sy
+    qz = cr * cp * sy - sr * sp * cy
+
+    return qx, qy, qz, qw
