94c8623c9a9df854c8e21507a3e2a115f4bddc4b
josedallatorre <jose.dallatorre@hotmail.com>
2026-03-30 16:26:32 +0200

    feat: adding comments 3

--- src/pitchside_tracker/pitchside_tracker/utils/yaml_loader.py
+++ src/pitchside_tracker/pitchside_tracker/utils/yaml_loader.py
@@ -2,7 +2,7 @@ import yaml
 from ament_index_python.packages import get_package_share_directory
 import os
 def load_yaml():
-    # --- Load YAML config ---
+    # Load YAML config
     config_path = os.path.join(
         get_package_share_directory('pitchside_tracker'),
         'config',
