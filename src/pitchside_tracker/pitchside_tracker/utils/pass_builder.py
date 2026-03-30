eb1d5c413f74a1e2a468e80a6b239ff29c933096
josedallatorre <jose.dallatorre@hotmail.com>
2026-03-30 16:25:23 +0200

    feat: adding comments 2

--- src/pitchside_tracker/pitchside_tracker/utils/pass_builder.py
+++ src/pitchside_tracker/pitchside_tracker/utils/pass_builder.py
@@ -1,5 +1,6 @@
 from pitchside_tracker.utils.geometry_utils import normalize
 
+# Helper function that creates the positions the ball will reach
 def build_pass_positions(player_positions, ball_height=0.3, offset=0.8):
     n = len(player_positions)
     pass_positions = []
@@ -12,7 +13,8 @@ def build_pass_positions(player_positions, ball_height=0.3, offset=0.8):
         ux, uy = normalize(dx, dy)
 
         # Kick originates just in front of the SENDER (player i)
-        kick_x = x1 + ux * offset
+        # The offset is used to space out ball position from the player position
+        kick_x = x1 + ux * offset 
         kick_y = y1 + uy * offset
         pass_positions.append((kick_x, kick_y, ball_height))
 
